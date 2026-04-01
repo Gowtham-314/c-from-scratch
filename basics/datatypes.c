7f3e7dc1f9041c8cc710280743bcb971c9c4cbe0
Gowtham Gowda C B <gowdagowtham1025@gmail.com>
2026-04-01 23:58:14 +0530

    Add VSCode C configs and initial C program
    Add project IDE configuration and first example program: new .vscode files (c_cpp_properties.json, launch.json, settings.json) configured for MinGW/GCC and gdb debugging, plus basics/first-pro.c (simple Hello World + scanf example) and its compiled binary at basics/output/first-pro.exe. These files set up IntelliSense, build/run/debug defaults and provide an initial runnable demo for the repository.

---
+++ basics/datatypes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    long e;
+    short f;
+    char str[100];
+    long int
+    return 0;
+}
---
+++ basics/first-pro.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+// Siingle line comment
+/*
+Multi line comment
+*/
+int main()
+{
+    int a = 10;
+    printf("Hello World\n");
+    printf("Enter a number: \n");
+    scanf("%d", &a);
+    printf("You entered: %d", a);
+    return 0;
+}
