29b5d676801ec7d5cddbe3566ebdf6238042b2cc
Gowtham Gowda C B <gowdagowtham1025@gmail.com>
2026-04-02 00:06:38 +0530

    Fix printf statements and add datatype outputs
    Fix missing semicolons and duplicate/malformed printf calls in basics/datatypes.c; add formatted printouts for float, char, double, long, short, string, long int, and long long int variables. Also add the compiled binary basics/output/datatypes.exe to the repository.

--- basics/datatypes.c
+++ basics/datatypes.c
@@ -13,8 +13,15 @@ int main()
     long int j=12345678910;
     long long int k10=123456789123456789;
     
-    printf("int a : %d \n",a)
-    printf("int a : %d \n",a)
+    printf("int a : %d \n",a);
+    printf("float b : %f \n",b);
+    printf("char c : %c \n",c);
+    printf("double d : %lf \n",d);
+    printf("long e : %ld \n",e);
+    printf("short f : %hd \n",f);
+    printf("char str : %s \n",str);
+    printf("long int j : %ld \n",j);
+    printf("long long int k10 : %lld \n",k10);
 
     return 0;
 }
