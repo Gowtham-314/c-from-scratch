af6a59d2237aa8698b0478219f74aa361a894521
Gowtham Gowda C B <gowdagowtham1025@gmail.com>
2026-04-02 00:01:03 +0530

    Update datatypes.c

--- basics/datatypes.c
+++ basics/datatypes.c
@@ -3,13 +3,18 @@
 
 int main()
 {
-    int a;
-    float b;
-    char c;
-    double d;
-    long e;
-    short f;
-    char str[100];
-    long int
+    int a=200;
+    float b=3.142312;
+    char c='A';
+    double d=2.7151648;
+    long e=10000000000;
+    short f=32767;
+    char str[100]="This is a @Gowtham-314";
+    long int j=12345678910;
+    long long int k10=123456789123456789;
+    
+    printf("int a : %d \n",a)
+    printf("int a : %d \n",a)
+
     return 0;
 }
