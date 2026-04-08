1455e686ec2eb03f7cc4e08f79079f8e38d432b0
Gowtham Gowda C B <gowdagowtham7930@gmail.com>
2026-04-08 23:50:22 +0530

    Add C examples for control statements
    Add basics/controls-in-c.c demonstrating use of control statements in C: break and continue in loops, exit() to terminate on negative input, and goto with labeled branches for negative/positive/zero cases. The program prints examples to stdout and interacts via scanf for the exit and goto demonstrations.

---
+++ basics/controls-in-c.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // Control statements
+    int a=10, b=20;
+
+    // break statement
+    printf("Break statement:\n");
+    for(int i=1; i<=10; i++)
+    {
+        if(i==5)
+        {
+            break;
+        }
+        printf("%d ", i);
+    }
+    printf("\n");
+    // continue statement
+    printf("\nContinue statement:\n");
+    for(int i=1; i<=10; i++)
+    {
+        if(i==5)
+        {
+            continue;
+        }
+        printf("%d ", i);
+    }
+    printf("\n");
+
+
+    // goto statement
+    printf("\nGoto statement:\n");
+    int x;
+    printf("Enter a number: "); 
+    scanf("%d", &x);
+    if(x < 0)
+    {
+        goto negative;
+    }
+    else if(x > 0)
+    {
+        goto positive;
+    }
+    else
+    {
+        goto zero;
+    }
+negative:
+    printf("You entered a negative number.\n");
+    goto end;
+positive:
+    printf("You entered a positive number.\n");
+    goto end;
+zero:
+    printf("You entered zero.\n");
+end:
+    return 0;
+}
