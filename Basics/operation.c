7cb48984ec467dea714c968a58e6d888ac06a14d
Gowtham Gowda C B <logicdraftlabs@gmail.com>
2026-05-27 23:37:26 +0530

    Create operation.c

---
+++ Basics/operation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int a, b, sum, difference, product, quotient;
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    
+    return 0;
+}
