1fb4d17c3c338cc585f2932c53bf6908ef7a3ef1
Gowtham Gowda C B <gowdagowtham7930@gmail.com>
2026-05-30 23:33:22 +0530

    Add pointerinstruct.exe and tidy C sources

--- Pointers/Structures/pointerinstruct.c
+++ Pointers/Structures/pointerinstruct.c
@@ -9,12 +9,15 @@ int main()
         int x;
         int y;
     };
+
     struct Point p1;
     struct Point *ptr;
+
     ptr = &p1;
     printf("Enter the x and y coordinates of the point: ");
     scanf("%d %d", &ptr->x, &ptr->y);
     printf("Point Details:\n");
     printf("Coordinates: (%d, %d)\n", ptr->x, ptr->y);
     return 0;
+    
 }
--- Pointers/Structures/structure.c
+++ Pointers/Structures/structure.c
@@ -9,9 +9,11 @@ int main()
         int y;
         char label[20];
     };
+
     struct Point p1;
     p1.x = 10;
     p1.y = 20;
+    
     printf("Enter the label for the point: ");
     scanf("%s", p1.label);
     printf("Point Details:\n");
