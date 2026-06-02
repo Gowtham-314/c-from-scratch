4c994c18beaaadcd9f0131fb430a402ad4129022
Gowtham Gowda C B <gowdagowtham7930@gmail.com>
2026-06-02 23:40:41 +0530

    Update union example and debug launch paths

--- Pointers/union/union.c
+++ Pointers/union/union.c
@@ -16,9 +16,19 @@ int main()
     union Data d;
     d.i = 10;
     printf("Value of i: %d\n", d.i);
+    // d = 10;
+
     d.f = 10.5;
     printf("Value of f: %.2f\n", d.f);
+    // d= 10.5;
+
     strcpy(d.str, "Hello");
     printf("Value of str: %s\n", d.str);
+    // d = "Hello";
+
+
+    printf("Value of i after modifying f: %d\n", d.i);
+
     return 0;
+
 }
